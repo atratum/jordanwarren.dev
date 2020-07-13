090aacc87d63442ed6e0a15032bef485773b7b39
Jordan Warren <atratum@protonmail.com>
2020-07-13 18:20:04 +0100

    Remove legacy logic, amend formatting, and add copyleft

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,67 +1,57 @@
 import React from "react"
 import { Link } from "gatsby"
 
-import { rhythm, scale } from "../utils/typography"
+import { rhythm } from "../utils/typography"
 
-const Layout = ({ location, title, children }) => {
-  const rootPath = `${__PATH_PREFIX__}/`
-  let header
+import Container from "../components/container";
 
-  if (location.pathname === rootPath) {
-    header = (
-      <h1
-        style={{
-          ...scale(1.5),
-          marginBottom: rhythm(1.5),
-          marginTop: 0,
-        }}
-      >
-        <Link
-          style={{
-            boxShadow: `none`,
-            color: `inherit`,
-          }}
-          to={`/`}
-        >
-          {title}
-        </Link>
-      </h1>
-    )
-  } else {
-    header = (
-      <h3
-        style={{
-          fontFamily: `Montserrat, sans-serif`,
-          marginTop: 0,
-        }}
-      >
-        <Link
-          style={{
-            boxShadow: `none`,
-            color: `inherit`,
-          }}
-          to={`/`}
-        >
-          {title}
-        </Link>
-      </h3>
-    )
-  }
+const Layout = ({ title, children }) => {
+
   return (
-    <div
-      style={{
-        marginLeft: `auto`,
-        marginRight: `auto`,
-        maxWidth: rhythm(24),
-        padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
-      }}
-    >
-      <header>{header}</header>
-      <main>{children}</main>
-      <footer>
-        © {new Date().getFullYear()}, Built with
-        {` `}
-        <a href="https://www.gatsbyjs.org">Gatsby</a>
+    <div style={{
+      height: `100%`,
+      display: `flex`,
+      flexDirection: `column`
+    }}>
+      <header style={{
+        paddingTop: `${rhythm(1)}`,
+        paddingBottom: `${rhythm(1)}`,
+      }}>
+        <Container>
+          <Link
+            style={{
+              boxShadow: `none`,
+              color: `inherit`,
+            }}
+            to={`/`}
+          >
+            {title}
+          </Link>
+        </Container>
+      </header>
+      <main style={{
+        paddingTop: `${rhythm(1)}`,
+        paddingBottom: `${rhythm(1)}`,
+        flex: 1
+      }}>
+        <Container>
+          {children}
+        </Container>
+      </main>
+      <footer style={{
+        paddingTop: `${rhythm(1)}`,
+        paddingBottom: `${rhythm(1)}`,
+      }}>
+        <Container>
+          <a
+          href="https://www.copyleft.org/"
+          rel="nofollow"
+          style={{
+            display: `inline-block`,
+            transform: `rotateY(180deg)`,
+          }}>©</a>&nbsp;
+          {new Date().getFullYear()}
+        </Container>
       </footer>
     </div>
   )
